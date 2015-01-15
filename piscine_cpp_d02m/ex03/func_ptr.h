3f7948c36dc0aec4daa6702eae41c2b4b39f6308
ChypRi0tE <nicolastem@hotmail.fr>
2015-01-15 17:10:30 +0100

    Update func_ptr.h

--- piscine_cpp_d02m/ex03/func_ptr.h
+++ piscine_cpp_d02m/ex03/func_ptr.h
@@ -1,13 +1,3 @@
-/*
-** func_ptr.h for piscine in /home/temeni_n/rendu/piscine_cpp_d02/ex03
-** 
-** Made by Nicolas Temenides
-** Login   <temeni_n@epitech.net>
-** 
-** Started on  Thu Jan  9 12:07:32 2014 Nicolas Temenides
-** Last update Thu Jan  9 12:44:27 2014 Nicolas Temenides
-*/
-
 #include	"func_ptr_enum.h"
 
 #ifndef		FUNC_PTR_H_
