135d1bfc760305c3291f1b9fb2358915e192d6ee
ChypRi0tE <nicolastem@hotmail.fr>
2015-01-15 17:09:54 +0100

    Update String.c

--- piscine_cpp_d03/ex00/String.c
+++ piscine_cpp_d03/ex00/String.c
@@ -1,13 +1,3 @@
-/*
-** String.c for d03 in /home/temeni_n/rendu/piscine_cpp_d03/ex00
-** 
-** Made by Nicolas Temenides
-** Login   <temeni_n@epitech.net>
-** 
-** Started on  Fri Jan 10 08:14:26 2014 Nicolas Temenides
-** Last update Fri Jan 10 10:37:31 2014 Nicolas Temenides
-*/
-
 #include	<stdlib.h>
 #include	"String.h"
 
