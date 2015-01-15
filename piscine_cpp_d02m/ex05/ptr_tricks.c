1117e4f6b25ff2278175a533f5823aad4a59822b
ChypRi0tE <nicolastem@hotmail.fr>
2015-01-15 17:09:41 +0100

    Update ptr_tricks.c

--- piscine_cpp_d02m/ex05/ptr_tricks.c
+++ piscine_cpp_d02m/ex05/ptr_tricks.c
@@ -1,13 +1,3 @@
-/*
-** ptr.tricks.c for ptr_tricks in /home/temeni_n/rendu/piscine_cpp_d02/ex05
-** 
-** Made by Nicolas Temenides
-** Login   <temeni_n@epitech.net>
-** 
-** Started on  Thu Jan  9 16:29:15 2014 Nicolas Temenides
-** Last update Thu Jan  9 16:50:09 2014 Nicolas Temenides
-*/
-
 #include	"ptr_tricks.h"
 
 int	get_array_nb_elem(int *ptr1, int *ptr2)
