5012daa8bce6fdb5f6ff9fe5dcec179a8dc95b35
ChypRi0tE <nicolastem@hotmail.fr>
2015-01-15 17:10:35 +0100

    Update add.c

--- piscine_cpp_d02m/ex04/add.c
+++ piscine_cpp_d02m/ex04/add.c
@@ -1,13 +1,3 @@
-/*
-** add.c for castmania in /home/temeni_n/rendu/piscine_cpp_d02/ex04
-** 
-** Made by Nicolas Temenides
-** Login   <temeni_n@epitech.net>
-** 
-** Started on  Thu Jan  9 14:52:22 2014 Nicolas Temenides
-** Last update Thu Jan  9 14:54:02 2014 Nicolas Temenides
-*/
-
 #include	"castmania.h"
 
 #define		ABS(x) ((x) > 0 ? (x) : (-x))
